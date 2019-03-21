abfd2c02c914a7e1051c186f7f843f8895a33741
Paul Kravchenko <pk@sitecore.net>
2019-03-21 17:35:03 +0200

    ItemResolver won't override true value of the HttpRequestArgs.PermissionDenied

---
+++ src/Sitecore.Support.257275/Pipelines/HttpRequest/ItemResolver.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.Pipelines.HttpRequest;
+
+namespace Sitecore.Support.Pipelines.HttpRequest
+{
+  public class ItemResolver:Sitecore.Pipelines.HttpRequest.ItemResolver
+  {
+    public override void Process(HttpRequestArgs args)
+    {
+      bool permissionDenied = args.PermissionDenied;
+      base.Process(args);
+      args.PermissionDenied |= permissionDenied;
+    }
+  }
+}
